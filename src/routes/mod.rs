a302c78e2aec6dd4ce58d98b895e940c9cc647a0
kutoru <eg_bls@mail.ru>
2024-06-21 18:38:45 +0300

    Merge notes router & Add auth layer

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,11 +1,11 @@
-use axum::{Router, http::{Method, header}, middleware::{Next, self}, response::Response};
+use axum::{Router, http::{Method, header, StatusCode}, middleware::{Next, self}, response::Response};
 use axum_extra::extract::CookieJar;
 use tower_http::cors::CorsLayer;
 
 use crate::types::AppState;
 
 mod auth;
-// mod notes;
+mod notes;
 
 pub fn get_router(state: &AppState) -> anyhow::Result<Router> {
     let origins = [
@@ -19,26 +19,49 @@ pub fn get_router(state: &AppState) -> anyhow::Result<Router> {
         .allow_credentials(true);
 
     let auth_router = auth::get_router(state);
-    // let notes_router = notes::get_router(state);
+    let notes_router = notes::get_router(state);
 
     Ok(
         Router::new()
-            .merge(auth_router)
-            // .merge(notes_router)
+            .merge(
+                auth_router
+            )
+            .merge(
+                notes_router.route_layer(middleware::from_fn(auth_mw))
+            )
             .layer(cors)
-            .route_layer(middleware::from_fn(middleware))
+            .route_layer(middleware::from_fn(log_mw))
     )
 }
 
-async fn middleware(
+async fn auth_mw(
     jar: CookieJar,
+    mut req: axum::extract::Request,
+    next: Next,
+) -> Result<Response, StatusCode> {
+    let token = match jar.get("at") {
+        Some(c) => {
+            println!("AT TOKEN: {:?}", c.value());
+            c.value()
+        },
+        None => {
+            println!("NO AT TOKEN");
+            return Err(StatusCode::UNAUTHORIZED);
+        },
+    };
+
+    // extract user id from the token
+    let user_id = 2;
+
+    req.extensions_mut().insert(user_id);
+    Ok(next.run(req).await)
+}
+
+async fn log_mw(
     req: axum::extract::Request,
     next: Next,
 ) -> Response {
     // let now = chrono::Local::now().format("%Y-%m-%d %H:%M:%S");
-    println!("\n{} -> {}", req.method(), req.uri());
-
-    println!("AT COOKIE: {:?}", jar.get("at"));
-
+    println!("\nRequest: {} -> {}", req.method(), req.uri());
     next.run(req).await
 }
