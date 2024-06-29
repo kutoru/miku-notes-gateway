2c3f72e93aa7ba4d3bfff86dddecd64159bd014d
kutoru <eg_bls@mail.ru>
2024-06-29 22:04:06 +0300

    Update tonic config

--- build.rs
+++ build.rs
@@ -1,14 +1,20 @@
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
-        .build_server(true)
+        .build_client(true)
+        .build_server(false)
         .type_attribute(".", "#[derive(serde::Serialize)]")
         .type_attribute(".", "#[derive(serde::Deserialize)]")
         .field_attribute("notes.CreateNoteReq.user_id", "#[serde(default)]")
         .field_attribute("notes.UpdateNoteReq.id", "#[serde(default)]")
         .field_attribute("notes.UpdateNoteReq.user_id", "#[serde(default)]")
         .compile(
-            &["./proto/sso.proto", "./proto/notes.proto", "./proto/tags.proto", "./proto/files.proto"],
+            &[
+                "./proto/sso.proto",
+                "./proto/notes.proto",
+                "./proto/tags.proto",
+                "./proto/files.proto",
+            ],
             &["./proto"],
         )?;
 
