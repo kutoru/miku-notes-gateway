b46ce4bebebac7d1a1b7a226e14e23d8e41e0c68
kutoru <eg_bls@mail.ru>
2024-07-01 06:16:22 +0300

    Update proto & Add file download route

--- src/routes/files.rs
+++ src/routes/files.rs
@@ -1,8 +1,13 @@
-use crate::proto::files::{CreateFileMetadata, CreateFileReq, DeleteFileReq, File, Empty};
+use crate::proto::files::{CreateFileMetadata, CreateFileReq, DeleteFileReq, DownloadFileReq, Empty, File, FileData};
 use crate::types::{call_grpc_service, new_ok_res};
 use crate::{types::{AppState, ServerResult, MultipartRequest}, error::ResError};
 
+use axum::body::Body;
+use axum::http::header;
+use axum::response::IntoResponse;
+use axum::routing::delete;
 use axum::{Router, routing::{post, get}, extract::{DefaultBodyLimit, State, Path}, Extension, http::StatusCode};
+use tokio_stream::StreamExt;
 use tower_http::limit::RequestBodyLimitLayer;
 use axum_typed_multipart::TypedMultipart;
 use tokio::io::AsyncReadExt;
@@ -14,8 +19,16 @@ pub fn get_router(state: &AppState) -> Router {
         .layer(RequestBodyLimitLayer::new(
             1024 * 1024 * state.req_body_limit,
         ))
-        .route("/files/:id", get(files_get).delete(files_delete))
+        .route("/files/:id", delete(files_delete))
+        .route("/files/dl/:hash", get(files_dl_get))
         .with_state(state.clone())
+}
+
+// impl to convert tonic stream directly into axum body (files_dl_get)
+impl From<FileData> for axum::body::Bytes {
+    fn from(value: FileData) -> Self {
+        value.data.into()
+    }
 }
 
 async fn files_post(
@@ -87,15 +100,49 @@ async fn files_post(
     new_ok_res(StatusCode::CREATED, new_file)
 }
 
-async fn files_get(
+async fn files_dl_get(
     State(mut state): State<AppState>,
-    Path(file_id): Path<i32>,
+    Path(file_hash): Path<String>,
     Extension(user_id): Extension<i32>,
-) -> ServerResult<File> {
+) -> impl IntoResponse {
 
-    println!("files_get with file_id and user_id: {}, {}", file_id, user_id);
+    println!("files_dl_get with file_hash and user_id: {}, {}", file_hash, user_id);
 
-    Err(ResError::NotImplemented("".into()))
+    let mut stream = call_grpc_service(
+        DownloadFileReq { user_id: user_id, file_hash: file_hash },
+        |req| state.files_client.download_file(req),
+        &state.data_token,
+    ).await?;
+
+    let first_part = stream.next().await
+        .ok_or(ResError::ServerError("Server error".into()))??;
+
+    let (file_name, file_size) = match first_part.metadata {
+        Some(m) => (m.name, m.size),
+        None => return Err(ResError::ServerError("Server error".into())),
+    };
+
+    let content_type = mime_guess::from_path(&file_name).first_raw()
+        .ok_or(ResError::ServerError("Server error".into()))?;
+
+    let body = Body::from_stream(stream);
+
+    let headers = [
+        (
+            header::CONTENT_TYPE,
+            content_type,
+        ),
+        (
+            header::CONTENT_LENGTH,
+            &file_size.to_string(),
+        ),
+        (
+            header::CONTENT_DISPOSITION,
+            &format!("attachment; filename=\"{}\"", file_name),
+        ),
+    ];
+
+    Ok((headers, body).into_response())
 }
 
 async fn files_delete(
